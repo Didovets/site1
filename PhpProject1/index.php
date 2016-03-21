979cae542860d41a6ac0b52deec8cb0a835b6261
andrii <andrii@andrii-HP-ProBook-4540s>
2016-03-21 17:56:57 +0200

    sss

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -1,27 +1,43 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
-  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
- <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Две колонки</title>
-  <style type="text/css">
-   .layout {
-    width: 100%; /* Ширина всей таблицы в процентах */
-   }
-   .layout TD {
-    vertical-align: top; /* Вертикальное выравнивание в ячейках */
-   }
-   TD.leftcol {
-    width: 200px; /* Ширина левой колонки в пикселах */
-   }
-  </style>
- </head>
- <body>
-  <table cellspacing="0" cellpadding="0" class="layout">
-   <tr> 
-    <td class="leftcol">Левая колонка</td>
-    <td>Правая колонка</td>
-   </tr>
-  </table>
- </body>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+    
+$rows = 50; // количество строк, tr
+$cols = 2; // количество столбцов, td
+
+echo '<table border="2">';
+
+for ($tr=1; $tr<=$rows; $tr+=1){ // в этом цикле счётчик $tr 
+    // следит за количеством строк и всегда равен текущему номеру строки.
+    // То есть в начале $tr=1, так как в начале у нас 1 строка, затем
+    // каждый раз прибавляем единицу, пока не дойдём до заданного количества
+    // $rows.
+echo '<tr>';
+if ($tr%2==0)        continue;
+if ($tr% 2 == 0) {
+  cols[Count_row].style.backgroundColor="Limegreen"
+  cols[Count_row].style.color="Darkred"
+ }
+ else {
+  cols[Count_row].style.backgroundColor="Yellow"
+  cols[Count_row].style.color="Blue"
+ }
+for ($td=0; $td<$cols; $td++) // в этом цикле счётчик $td аналогичен
+                                    // счётчику $tr. 
+echo '<td>'. ($tr+$td) .'</td>';
+echo '</tr>';
+}
+echo '</table>';  
+        ?>
+    </body>
 </html>
